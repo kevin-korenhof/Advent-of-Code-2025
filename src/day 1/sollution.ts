b733b2174a9791144d62de385750c8ed552df174
Kevin Korenhof <kevin.korenhof@cito.nl>
2025-12-01 16:04:32 +0100

    refactor

--- src/day 1/sollution.ts
+++ src/day 1/sollution.ts
@@ -8,19 +8,36 @@ let value = dialStart
 
 let timesZero = 0
 
-const parsedData = data.split('\n')
+type data = {
+    number: number,
+    direction: string,
+    fullRotations?: number
+}
+
+function getFullRotations(turns:number){
+    let rotations:number
+    if(turns > 100){
+        rotations = Number(turns.toString().slice(0,1))
+    }else(
+        rotations = 0
+    )
+    return rotations
+}
+
+const parsedData:data[] = data.split('\n').map(datapoint => ({
+    number: Number(datapoint.slice(1)) as number,
+    direction: datapoint.slice(0,1),
+
+})
+)
 
 const testdata = ["L250", "L410", "r10", "R910", "L310"]
 
-testdata.map((datapoint) =>{
-    const number = Number(datapoint.slice(1))
-    const directions = datapoint.slice(0,1)
-})
-console.log(testdata[0])
+console.log(parsedData[0])
 
 
 // testdata.forEach((dataPoint) =>{
-//     const startValue = value
+// const startValue = value
 // let fullRotations = 0
 // let number = Number(dataPoint.slice(1))
 // const direction = dataPoint.slice(0,1)
@@ -61,39 +78,37 @@ console.log(testdata[0])
 // console.log('times zero ' + timesZero)
 // })
 
-// parsedData.forEach((dataPoint) =>{
-// const startValue = value
-// let fullRotations = 0
-// let number = Number(dataPoint.slice(1))
-// const direction = dataPoint.slice(0,1)
+parsedData.forEach(({number, direction}) =>{
+const startValue = value
+let fullRotations = 0
 
-// if (number >= 100){
-//     number = Number(number.toString().slice(1))
-//     fullRotations = Number(dataPoint.toString().slice(1,2))
-//     timesZero = timesZero + fullRotations
-// }
+if (number >= 100){
+    fullRotations = Number(number.toString().slice(0,1))
+    number = Number(number.toString().slice(1))
+    timesZero = timesZero + fullRotations
+}
 
-// if(direction == "L") {
-//     turn = Number(number * -1)
-// } else {
-//     turn = Number(number)
-// }
+if(direction == "L") {
+    turn = Number(number * -1)
+} else {
+    turn = Number(number)
+}
 
-// value = Number(value + turn)
-// if (value < dialMin){
-//     value = value+100
-//     if(startValue!=0){
-//     timesZero++}
+value = Number(value + turn)
+if (value < dialMin){
+    value = value+100
+    if(startValue!=0){
+    timesZero++}
 
-// }
-// if (value > dialMax){
-//     value = value-100
-//     return timesZero++
-// }
+}
+if (value > dialMax){
+    value = value-100
+    return timesZero++
+}
 
-// if (value === 0) {
-//     return timesZero++
-// }
+if (value === 0) {
+    return timesZero++
+}
 
-// })
-// console.log(timesZero) // sollution: 5933
+})
+console.log(timesZero) // sollution: 5933
