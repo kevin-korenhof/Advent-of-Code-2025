9a88b53b8cdc93d43d542771bf4b2a6bd85d4c82
Kevin Korenhof <kevin.korenhof@incentro.com>
2025-12-01 20:37:06 +0100

    More cleanup

--- src/day 1/solution.ts
+++ src/day 1/solution.ts
@@ -1,59 +1,55 @@
 import data from "./data.ts";
 
 type data = {
-  number: number;
+  netTurns: number;
   direction: string;
   fullRotations: number;
 };
 
 const { dialStart, dialMax, dialMin, input } = data;
 
-let turns = 0;
-let value = dialStart;
+// starting values
+let currentValue = dialStart;
 let timesZero = 0;
 
 // helper functions
-function getFullRotations(turns: number) {
-  let rotations: number;
-  if (turns > 100) {
-    rotations = Number(turns.toString().slice(0, 1));
-  } else rotations = 0;
-  return rotations;
-}
-
 function parseData(data: string): data[] {
   const splitData: data[] = data.split("\n").map((dataPoint) => {
-    let number = Number(dataPoint.slice(1));
-    let direction = dataPoint.slice(0, 1);
-    let fullRotations = getFullRotations(number);
-    return { number, fullRotations, direction };
+    const turns = Number(dataPoint.slice(1));
+    const direction = dataPoint.slice(0, 1);
+    const fullRotations = Math.floor(turns / 100);
+    const netTurns = turns - fullRotations * 100;
+
+    return { netTurns, fullRotations, direction };
   });
   return splitData;
 }
 
 // Actual solution
-parseData(input).forEach(({ number, direction, fullRotations }) => {
-  const startValue = value;
-  timesZero = timesZero + fullRotations;
+parseData(input).forEach(({ netTurns, direction, fullRotations }) => {
+  const startValue = currentValue;
 
-  const netTurns = number >= 100 ? Number(number.toString().slice(1)) : number;
-
+  // set 'direction' for the turns
   const turns = direction == "L" ? netTurns * -1 : netTurns;
 
-  value = value + turns;
-  if (value < dialMin) {
-    value = value + 100;
+  // add full rotations to timesZero
+  timesZero = timesZero + fullRotations;
+  currentValue = currentValue + turns;
+
+  if (currentValue < dialMin) {
+    currentValue = currentValue + 100;
     if (startValue != 0) {
+      // if we did not start at 0, it means we passed it. Otherwise it was already counted
       timesZero++;
     }
   }
 
-  if (value > dialMax) {
-    value = value - 100;
+  if (currentValue > dialMax) {
+    currentValue = currentValue - 100;
     return timesZero++;
   }
 
-  if (value == 0) {
+  if (currentValue == 0) {
     return timesZero++;
   }
 });
