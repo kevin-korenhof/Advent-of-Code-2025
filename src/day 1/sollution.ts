6e76258701e9c39b2c4ee7c3eae5539a85e829b6
Kevin Korenhof <kevin.korenhof@cito.nl>
2025-12-01 16:04:31 +0100

    Small refactor

--- src/day 1/sollution.ts
+++ src/day 1/sollution.ts
@@ -10,10 +10,13 @@ let timesZero = 0
 
 const parsedData = data.split('\n')
 
-
-
 const testdata = ["L250", "L410", "r10", "R910", "L310"]
 
+testdata.map((datapoint) =>{
+    const number = Number(datapoint.slice(1))
+    const directions = datapoint.slice(0,1)
+})
+console.log(testdata[0])
 
 
 // testdata.forEach((dataPoint) =>{
@@ -58,39 +61,39 @@ const testdata = ["L250", "L410", "r10", "R910", "L310"]
 // console.log('times zero ' + timesZero)
 // })
 
-parsedData.forEach((dataPoint) =>{
-    const startValue = value
-let fullRotations = 0
-let number = Number(dataPoint.slice(1))
-const direction = dataPoint.slice(0,1)
+// parsedData.forEach((dataPoint) =>{
+// const startValue = value
+// let fullRotations = 0
+// let number = Number(dataPoint.slice(1))
+// const direction = dataPoint.slice(0,1)
 
-if (number >= 100){
-    number = Number(number.toString().slice(1))
-    fullRotations = Number(dataPoint.toString().slice(1,2))
-    timesZero = timesZero + fullRotations
-}
+// if (number >= 100){
+//     number = Number(number.toString().slice(1))
+//     fullRotations = Number(dataPoint.toString().slice(1,2))
+//     timesZero = timesZero + fullRotations
+// }
 
-if(direction == "L") {
-    turn = Number(number * -1)
-} else {
-    turn = Number(number)
-}
+// if(direction == "L") {
+//     turn = Number(number * -1)
+// } else {
+//     turn = Number(number)
+// }
 
-value = Number(value + turn)
-if (value < dialMin){
-    value = value+100
-    if(startValue!=0){
-    timesZero++}
+// value = Number(value + turn)
+// if (value < dialMin){
+//     value = value+100
+//     if(startValue!=0){
+//     timesZero++}
 
-}
-if (value > dialMax){
-    value = value-100
-    return timesZero++
-}
+// }
+// if (value > dialMax){
+//     value = value-100
+//     return timesZero++
+// }
 
-if (value === 0) {
-    return timesZero++
-}
+// if (value === 0) {
+//     return timesZero++
+// }
 
-})
-console.log(timesZero) // sollution: 5933
+// })
+// console.log(timesZero) // sollution: 5933
