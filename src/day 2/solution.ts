76b984dec06903b68ce319960f24f08ff9e0b555
Kevin Korenhof <kevin.korenhof@cito.nl>
2025-12-02 13:03:25 +0100

    Small improvement

--- src/day 2/solution.ts
+++ src/day 2/solution.ts
@@ -1,42 +1,44 @@
 import { rawData, testData } from "./data.ts";
 
-type Data ={
-    startValue: number,
-    endValue: number
+console.time("Tijdsduur");
+
+type Data = {
+  startValue: number;
+  endValue: number;
+};
+
+function parseData(data: string): Data[] {
+  const splitData: Data[] = data.split(",").map((dataPoint) => {
+    const splitDataPoint = dataPoint.split("-");
+    const startValue = Number(splitDataPoint[0]);
+    const endValue = Number(splitDataPoint[1]);
+    return { startValue, endValue };
+  });
+
+  return splitData;
 }
 
-function parseData(data:string):Data[] {
-    const splitData:Data[] = data.split(',').map((dataPoint) =>{
-        const splitDataPoint = dataPoint.split('-')
-        const startValue = Number(splitDataPoint[0])
-        const endValue = Number(splitDataPoint[1])
-        return {startValue, endValue}
-    })
+function splitNumber(input: number, maxLength: number): number[] {
+  let numbers: number[] = [];
+  const nmrLength = input.toString().length;
 
-    return splitData
-}
-
-function splitNumber(input:number, maxLength:number):number[] {
-    let numbers: number[] = []
-    const nmrLength = input.toString().length
-    
-    if(nmrLength % maxLength == 0) {
-        for(let i = 0; i<nmrLength;){
-            const endIndex = i + maxLength
-            numbers.push(Number(input.toString().substring(i,endIndex)))
-            i = endIndex
-        }
+  if (nmrLength % maxLength == 0) {
+    for (let i = 0; i < nmrLength; ) {
+      const endIndex = i + maxLength;
+      numbers.push(Number(input.toString().substring(i, endIndex)));
+      i = endIndex;
     }
-    return numbers
+  }
+  return numbers;
 }
 
 function assertEqualNumbers(numbers: number[]): boolean {
-   return numbers.every(val => val == numbers[0])
+  return numbers.every((val) => val == numbers[0]);
 }
 
-const parsedData: Data[] = parseData(rawData)
+const parsedData: Data[] = parseData(rawData);
 
-let result = 0
+let result = 0;
 
 // part 1:
 // parsedData.forEach(({startValue, endValue}) => {
@@ -44,7 +46,7 @@ let result = 0
 //         const valueLength = i.toString().length
 
 //         // Numbers with an odd amount of digits are always good, so only even will be processed
-//         if(valueLength % 2 == 0){    
+//         if(valueLength % 2 == 0){
 //         const firstHalf = i.toString().slice(0, valueLength/2)
 //         const secondHalf = i.toString().slice(valueLength/2, valueLength)
 
@@ -58,32 +60,26 @@ let result = 0
 
 // part 2:
 
-parsedData.forEach(({startValue, endValue}) => {
-    for(let i=startValue; i<=endValue; i++){
-        let addResult = 0
-        const valueLength = i.toString().length
-        const halfLength = valueLength/2
+parsedData.forEach(({ startValue, endValue }) => {
+  for (let i = startValue; i <= endValue; i++) {
+    let addResult = 0;
+    const valueLength = i.toString().length;
+    const halfLength = valueLength / 2;
 
-        for(let j=1; j<=halfLength; j++){
-            if((valueLength % j) == 0){
-                const splitted = splitNumber(i,j)
+    for (let j = 1; j <= halfLength; j++) {
+      if (valueLength % j == 0) {
+        const splitted = splitNumber(i, j);
 
-                if(assertEqualNumbers(splitted)){
-                    addResult = i
-                }
-            }
-        
+        if (assertEqualNumbers(splitted)) {
+          addResult = i;
+          break;
         }
-        result = result + addResult
+      }
     }
-})
+    result = result + addResult;
+  }
+});
 
-console.log(result)
+console.log(result);
 
-
-
-
-// part 1 answer: 28844599675 DONE!
-
-
-// Number(input.toString().substring(0,1))
+console.timeEnd("Tijdsduur");
