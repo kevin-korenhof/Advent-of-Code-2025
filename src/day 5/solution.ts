39330cfc8226ccaaf79aa83ee34c83edb3502391
Kevin Korenhof <kevin.korenhof@incentro.com>
2025-12-05 18:20:07 +0100

    day 5

---
+++ src/day 5/solution.ts
@@ -0,0 +1,83 @@
+/**
+ * range of id's with fresh ingrediënts
+ * Check list of ingrediënts if it is in 1 or more ranges
+ */
+
+import { testData, rawData } from "./data.ts";
+
+let answer = 0;
+
+// split data in ranges and ingrediënts
+
+const ranges = rawData
+  .split("\n\n")[0]
+  ?.split("\n")
+  .map((range) => {
+    const [minStr, maxStr] = range.split("-");
+
+    const min = Number(minStr);
+    const max = Number(maxStr);
+    return { min, max };
+  });
+
+const ingrediënts = rawData
+  .split("\n\n")[1]
+  ?.split("\n")
+  .map((ingredient) => {
+    const number = Number(ingredient);
+    return number;
+  });
+// optional: sort on min value
+
+const rangesSorted = ranges?.sort((a, b) => a.min - b.min);
+
+// check ingrediënts if part of a range.
+ingrediënts?.forEach((ingredient) => {
+  let fresh = false;
+  const ingredientNumber = Number(ingredient);
+  rangesSorted?.forEach(({ min, max }) => {
+    if (ingredient >= min && ingredient <= max) {
+      fresh = true; // no need to check other ranges as well, it's already known to be fresh
+    }
+  });
+  if (fresh) {
+    answer++;
+  }
+});
+
+console.log("Number of fresh ingrediënts: " + answer);
+
+/**
+ * part 2:
+ * Only consider the fres ranges:
+ * How many ingredient's are within those ranges to be fres?
+ */
+
+let secondAnswer = 0;
+
+function uniqueRange(arr: { min: number; max: number }[]) {
+  let res: {}[] = [];
+  console.log(arr);
+  arr?.forEach(({ min, max }, i) => {
+    let resMin = min;
+    let resMax = max;
+
+    if (res.length > 0 && res[res.length - 1].max >= resMax) {
+      return;
+    } else {
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[j].min <= resMax) resMax = Math.max(arr[j].max, resMax);
+      }
+    }
+
+    res.push({ min: resMin, max: resMax });
+  });
+  return res;
+}
+const sortRange = uniqueRange(rangesSorted);
+
+sortRange.forEach((range) => {
+  secondAnswer = secondAnswer + range.max - range.min + 1;
+});
+
+console.log(secondAnswer); // 343143696885053
