41f4df4da6ff378c3102712bfd5ab20aeaf68924
Kevin Korenhof <kevin.korenhof@cito.nl>
2025-12-09 17:15:41 +0100

    Still not workign though...

--- src/day 9/solution.ts
+++ src/day 9/solution.ts
@@ -83,25 +83,69 @@ let coveredTiles = [];
 
 xPaths.forEach((path, i) => {
   const y = path.y;
-  const start = path.start;
-  const eind = path.eind;
+  let start = path.start;
+  let eind = path.eind;
+
+  let addNewRange = true;
 
   if (i == 0) {
     const ranges = [{ start: start, eind: eind }];
     coveredTiles.push({ y, ranges });
   } else {
-    let ranges = [...coveredTiles[i - 1].ranges];
-    ranges.forEach((range) => {});
+    const previousRanges = [...coveredTiles[i - 1].ranges];
+    let ranges = [];
+    previousRanges.forEach((range) => {
+      if (start <= eind) {
+        // no overlap with new range
+        if (range.start > eind || range.eind < start) {
+          ranges.push(range);
+        } else {
+          // overlap with start of new range.
+          if (range.start < start && range.eind >= start) {
+            ranges.push({ start: range.start, eind: start - 1 });
+            start = range.eind + 1;
+          } else {
+            if (range.start <= eind && range.eind >= eind) {
+              ranges.push({ start: eind + 1, eind: range.eind });
+              eind = range.start - 1;
+            } else {
+              // full overlap, create 2 new ranges and don't add the 'new' range
+              if (range.start <= start && range.eind >= eind) {
+                ranges.push({ start: range.start, eind: start - 1 });
+                ranges.push({ start: eind + 1, eind: range.eind });
+
+                addNewRange = false;
+              } else {
+                if (range.start >= start && range.eind <= eind) {
+                  ranges.push({ start: start, eind: range.start - 1 });
+                  ranges.push({ start: range.eind + 1, eind: eind });
+                }
+              }
+            }
+          }
+        }
+      } else {
+        ranges.push(range);
+      }
+    });
+
+    if (addNewRange && start <= eind) {
+      ranges.push({ start: start, eind: eind });
+    }
+
+    ranges.sort((a, b) => a.start - b.start);
+
+    coveredTiles.push({ y, ranges });
   }
 });
 
 // Check if all values are covered
-dataRows.forEach((firstTile, i) => {
+dataRows.forEach((firstTile, k) => {
   const tile1 = firstTile.split(",");
   const x1 = Number(tile1[0]);
   const y1 = Number(tile1[1]);
 
-  for (let j = i + 1; j < dataRows.length; j++) {
+  for (let j = k + 1; j < dataRows.length; j++) {
     const tile2 = dataRows[j].split(",");
     const x2 = Number(tile2[0]);
     const y2 = Number(tile2[1]);
@@ -111,136 +155,50 @@ dataRows.forEach((firstTile, i) => {
     const ymin = Math.min(y1, y2);
     const ymax = Math.max(y1, y2);
 
-    if (surface > maxSurface) {
-      maxSurface = surface;
+    let ok = true;
+
+    let indexCheckMin = 0;
+    let indexCheckMax;
+
+    for (let l = 0; l < coveredTiles.length; l++) {
+      if (coveredTiles[l].y < ymin) {
+        indexCheckMin = l;
+      }
+      if (coveredTiles[l].y > ymax) {
+        indexCheckMax = l;
+        break;
+      }
+    }
+
+    for (let m = indexCheckMin; m <= indexCheckMax; m++) {
+      const rowRanges = coveredTiles[m].ranges;
+
+      let rowOk = false;
+
+      rowRanges.forEach((range) => {
+        const start = range.start;
+        const eind = range.eind;
+
+        if (start <= xmin && eind >= xmax) {
+          rowOk = true;
+        }
+      });
+
+      if (!rowOk) {
+        ok = false;
+        break;
+      }
+    }
+
+    if (ok) {
+      const surface = (Math.abs(x1 - x2) + 1) * (Math.abs(y1 - y2) + 1);
+      if (surface > maxSurface) {
+        maxSurface = surface;
+      }
     }
   }
 });
 
-// dataRows.forEach((row) => {
-//   const tile = row.split(",");
-//   const x = Number(tile[0]);
-//   const y = Number(tile[1]);
+console.log("secondAnswer: " + maxSurface); //917796 to low
 
-//   xmax = x > xmax ? x : xmax;
-//   xmin = x < xmin ? x : xmin;
-//   ymax = y > ymax ? y : ymax;
-//   ymin = y < ymin ? y : ymin;
-// });
-
-// let floorgrid = [];
-// let xrow = [];
-// for (let ix = 0; ix <= xmax + 1; ix++) {
-//   xrow.push(".");
-// }
-
-// for (let iy = 0; iy <= ymax + 1; iy++) {
-//   let row = [...xrow];
-//   floorgrid.push(row);
-//   console.log(iy);
-// }
-
-// // create outer lines
-// dataRows.forEach((row, i) => {
-//   const tile = row.split(",");
-//   const xTile = Number(tile[0]);
-//   const yTile = Number(tile[1]);
-//   floorgrid[yTile][xTile] = "R";
-
-//   const tile2Raw = i == dataRows.length - 1 ? dataRows[0] : dataRows[i + 1];
-//   const tile2 = tile2Raw?.split(",");
-//   const xTile2 = Number(tile2[0]);
-//   const yTile2 = Number(tile2[1]);
-
-//   if (xTile == xTile2) {
-//     if (yTile > yTile2) {
-//       for (let j = yTile; j > yTile2; j--) {
-//         floorgrid[j][xTile] != "R" ? (floorgrid[j][xTile] = "G") : "";
-//       }
-//     } else {
-//       for (let j = yTile; j < yTile2; j++) {
-//         floorgrid[j][xTile] != "R" ? (floorgrid[j][xTile] = "G") : "";
-//       }
-//     }
-//   }
-
-//   if (yTile == yTile2) {
-//     if (xTile > xTile2) {
-//       for (let j = xTile; j > xTile2; j--) {
-//         floorgrid[yTile][j] != "R" ? (floorgrid[yTile][j] = "G") : "";
-//       }
-//     } else {
-//       for (let j = xTile; j < xTile2; j++) {
-//         floorgrid[yTile][j] != "R" ? (floorgrid[yTile][j] = "G") : "";
-//       }
-//     }
-//   }
-// });
-
-// //fill in inner part of grid.
-// floorgrid.forEach((row, i) => {
-//   // get first G
-//   const indexG = row.indexOf("G") == -1 ? 99999999 : row.indexOf("G");
-//   // get first R
-//   const indexR = row.indexOf("R") == -1 ? 99999999 : row.indexOf("R");
-//   // get last '.'
-//   const indexDot = row.lastIndexOf(".");
-
-//   const firstTile = Math.min(indexG, indexR);
-
-//   if (firstTile > -1) {
-//     for (let t = firstTile + 1; t < indexDot; t++) {
-//       floorgrid[i][t] = "G";
-//     }
-//   }
-// });
-
-// dataRows.forEach((firstTile, i) => {
-//   const tile1 = firstTile.split(",");
-//   const x1 = Number(tile1[0]);
-//   const y1 = Number(tile1[1]);
-
-//   for (let j = i + 1; j < dataRows.length; j++) {
-//     const tile2 = dataRows[j].split(",");
-//     const x2 = Number(tile2[0]);
-//     const y2 = Number(tile2[1]);
-
-//     const xlow = Math.min(x1, x2);
-//     const xhigh = Math.max(x1, x2);
-//     const ylow = Math.min(y1, y2);
-//     const yhigh = Math.max(y1, y2);
-
-//     let gridOk = true;
-
-//     for (let ix = xlow; ix > xhigh; ix++) {
-//       if (floorgrid[ylow][ix] == ".") {
-//         gridOk = false;
-//       }
-//     }
-//     for (let iy = ylow; iy < yhigh; iy++) {
-//       if (floorgrid[iy][xlow] == ".") {
-//         gridOk = false;
-//       }
-//     }
-//     for (let ix = xhigh; ix < xlow; ix--) {
-//       if (floorgrid[yhigh][ix] == ".") {
-//         gridOk = false;
-//       }
-//     }
-//     for (let iy = yhigh; iy < ylow; iy--) {
-//       if (floorgrid[iy][xlow] == ".") {
-//         gridOk = false;
-//       }
-//     }
-
-//     if (gridOk) {
-//       const surface = (Math.abs(x1 - x2) + 1) * (Math.abs(y1 - y2) + 1);
-
-//       if (surface > maxSurface) {
-//         maxSurface = surface;
-//       }
-//     }
-//   }
-// });
-
-console.log("secondAnswer: " + maxSurface);
+// 3320172416 wrong
